ef90b1ac8dda6a893d816bdbd7cbd9c05db1ccea
Khairul <engmkbj@gmail.com>
2019-05-27 15:35:57 -0600

    convert to MVC design pattern

---
+++ Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  List Todoye
+//
+//  Created by Jahid Bashar on 27/5/19.
+//  Copyright © 2019 Jahid Bashar. All rights reserved.
+//
+
+import Foundation
---
+++ List Todoye/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  List Todoye
+//
+//  Created by Jahid Bashar on 27/5/19.
+//  Copyright © 2019 Jahid Bashar. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
